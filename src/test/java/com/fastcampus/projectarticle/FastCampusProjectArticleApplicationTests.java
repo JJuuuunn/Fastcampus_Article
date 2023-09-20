8bf7ec19261ccd086eeac40ded15e8b021d8d6c3
jjuuuunn <didtdjwns111@gmail.com>
2023-09-20 16:33:46 +0900

    #6 - 스프링 부트 프로젝트 시작
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬북

---
+++ src/test/java/com/fastcampus/projectarticle/FastCampusProjectArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectarticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCampusProjectArticleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
